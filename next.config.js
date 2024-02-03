3528a898b69223495281cd6080a5cd13c2908c3e
Gabriel Nastase <gabriel.nastase@nutmeg.com>
2024-02-03 22:37:25 +0000

    rm image optimisations

--- next.config.js
+++ next.config.js
@@ -13,7 +13,19 @@ module.exports = {
       'a0.awsstatic.com',
       'transitivebullsh.it'
     ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/b23a8ea6-3b63-4164-abc6-68b872545e43/**'
+      }
+    ],
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
+    deviceSizes: [1920],
+    imageSizes: [256],
+    minimumCacheTTL: 31536000,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
